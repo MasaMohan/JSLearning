d3cf0cfd38d97d58d939ebad80d950a3723e3c70
MasaMohan <masamohan14@gmail.com>
2023-09-23 02:16:13 +0530

    updated
    updated

---
+++ 02_typeOf.js
@@ -0,0 +1,23 @@
+
+
+
+console.log("========Assignment02=========");
+console.log("====Step01====");
+var bankName = "SBI Bank";
+console.log("Bank Name:=",bankName,"Type:-",typeof bankName);
+
+
+var mark = "90%";
+console.log("Marks:-90%",mark,typeof mark);
+
+var stay 
+console.log(typeof stay);
+stay = empty;
+console.log(stay);
+
+
+
+
+
+
+
---
+++ 02_variableBasicAssignment.js
@@ -0,0 +1,40 @@
+
+console.log("========Assignment01========");
+
+
+console.log("======String======");
+
+var fulName = "Mohan Masa"
+console.log("Full Name:-", fulName,"|| Type of:-", typeof fulName);
+var collageName = "California University"
+console.log("College Name:-", collageName,"|| Type of:-" ,typeof collageName);
+var city = "California"
+console.log("City California", "||Type of :-", typeof city);
+
+console.log("========Number========");
+
+var pinCode = 421306;
+console.log("PinCode:-",pinCode,"Type:-",typeof pinCode);
+var mobileNo = 9594525911;
+console.log("Mobile No:-",mobileNo, "Type:-",pinCode);
+var employeeId = 12527;
+console.log("Employee Id;-",employeeId,"Type:-",employeeId);
+
+console.log("========Boolean========");
+
+var married = true;
+console.log("Married Status",married,"Type:-",typeof married);
+var male = true;
+console.log("Male:-",male,"Type:-",typeof male);
+var available = true;
+console.log("Available:-",available,"Type:-",typeof available);
+
+console.log("====Difference between null vs undefined and log on console====");
+console.log("========undefined========");
+console.log("undefined=Variable is declare but value not initialization");
+console.log("========null========");
+console.log("null=value is given to variable when we want to keep value as a empty ");
+
+
+
+
