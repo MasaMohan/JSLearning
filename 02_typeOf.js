d58ad5de8156ece9d534f5f312420c18bb82b1f9
MasaMohan <masamohan14@gmail.com>
2023-09-23 22:26:53 +0530

    data  type updated
    data  type updated

--- 02_typeOf.js
+++ 02_typeOf.js
@@ -2,18 +2,56 @@
 
 
 console.log("========Assignment02=========");
+
 console.log("====Step01====");
+
+console.log("====Before Initialization type====");
+var bankName
+console.log("Bank Name","Data Type:-",typeof bankName);
+
+console.log("====After Initialization type==== ");
 var bankName = "SBI Bank";
-console.log("Bank Name:=",bankName,"Type:-",typeof bankName);
+console.log("Bank Name:=",bankName,"Data Type:-",typeof bankName);
 
 
+console.log("====Step 02====");
+
+var mark
+console.log("===Before Initialization type====");
+console.log("Data Type:-",typeof mark);
+
+console.log("===After Initialization type====");
 var mark = "90%";
-console.log("Marks:-90%",mark,typeof mark);
+console.log("Marks:-",mark,typeof mark);
 
-var stay 
-console.log(typeof stay);
-stay = ;
-console.log(stay);
+console.log("====Step 03====");
+
+var isWorking = true
+console.log("Data Type:-true",isWorking,typeof isWorking);
+
+console.log("====Step 04====");
+
+var totalCount = "Hundred and Seven";
+console.log("====Step 04 A ====");
+
+console.log("Total Count:-",totalCount,"Data Type:-",typeof totalCount);
+
+var totalCount = 107; // updated 
+console.log("====Step 04 B ===="); 
+
+console.log("New Value Updated:-",totalCount,"Data Type:-",typeof totalCount);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
