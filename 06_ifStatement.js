0e17d6c311258c6ec99c25f4a70db3b5250312b0
MasaMohan <masamohan14@gmail.com>
2023-10-02 23:57:33 +0530

    if else function assignment done
    if else function assignment done

---
+++ 06_ifStatement.js
@@ -0,0 +1,28 @@
+
+
+
+var girlAge = 19;
+if (girlAge>=18) {
+    console.log(`You could go inside office`);
+    console.log(`Have you informed your parent`);
+    console.log(`Signed.. Congrats!!!`);
+    
+}
+console.log(`Daily life`);
+
+console.log(`**********************************************************************
+`);
+
+// If age is greater than equal 18 then allow for vote else no,
+
+var elonAge = 17;
+if (elonAge>=18) {
+    console.log(`Welcome.. Elon`);
+    console.log(`Please let5 me mark your finger with ink`);
+    console.log(`Please now vote`);
+
+} else {
+    console.log(`Yet you are not eligible for vote`);
+    console.log(`Try next time please `);
+    
+}
